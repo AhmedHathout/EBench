e7a2f7650bd8a67b3ed2ae55cbcd7aafd4c7647e
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-11-26 10:54:52 +0000

    Adding tarfile member sanitization to extractall()

--- Server/install_prover/install_prover.py
+++ Server/install_prover/install_prover.py
@@ -11,7 +11,29 @@ class Installer(object):
         path = get_prover_path(self.prover_id)
         prover_tgz = path + "E.tgz"
         with tarfile.open(prover_tgz) as tar:
-            tar.extractall(path)
+            
+            import os
+            
+            def is_within_directory(directory, target):
+                
+                abs_directory = os.path.abspath(directory)
+                abs_target = os.path.abspath(target)
+            
+                prefix = os.path.commonprefix([abs_directory, abs_target])
+                
+                return prefix == abs_directory
+            
+            def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+            
+                for member in tar.getmembers():
+                    member_path = os.path.join(path, member.name)
+                    if not is_within_directory(path, member_path):
+                        raise Exception("Attempted Path Traversal in Tar File")
+            
+                tar.extractall(path, members, numeric_owner=numeric_owner) 
+                
+            
+            safe_extract(tar, path)
 
     def configure(self):
         path_to_configure_file = get_prover_path(self.prover_id) + "E/"
